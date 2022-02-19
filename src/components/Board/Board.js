dab9b9b76c3fb584533d4fefad7582b467fc9647
yakhal <rastogi.p39@gmail.com>
2022-02-19 20:39:10 +0530

    Game Logic implemented, fixes #1

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,23 +1,23 @@
 import Square from "./Square";
 import "./Board.css";
 
-function Board(props) {
+function Board(props) {    
     return (
         <div className="board">
             <div className="board-row">
-                <Square id={0} value={props.boardValues[0]} />
-                <Square id={1} value={props.boardValues[1]} />
-                <Square id={2} value={props.boardValues[2]} />
+                <Square id={0} value={props.boardValues[0]} changeGameState={props.changeGameState}/>
+                <Square id={1} value={props.boardValues[1]} changeGameState={props.changeGameState}/>
+                <Square id={2} value={props.boardValues[2]} changeGameState={props.changeGameState}/>
             </div>
             <div className="board-row">
-                <Square id={3} value={props.boardValues[3]} />
-                <Square id={4} value={props.boardValues[4]} />
-                <Square id={5} value={props.boardValues[5]} />
+                <Square id={3} value={props.boardValues[3]} changeGameState={props.changeGameState}/>
+                <Square id={4} value={props.boardValues[4]} changeGameState={props.changeGameState}/>
+                <Square id={5} value={props.boardValues[5]} changeGameState={props.changeGameState}/>
             </div>
             <div className="board-row">
-                <Square id={6} value={props.boardValues[6]} />
-                <Square id={7} value={props.boardValues[7]} />
-                <Square id={8} value={props.boardValues[8]} />
+                <Square id={6} value={props.boardValues[6]} changeGameState={props.changeGameState}/>
+                <Square id={7} value={props.boardValues[7]} changeGameState={props.changeGameState}/>
+                <Square id={8} value={props.boardValues[8]} changeGameState={props.changeGameState}/>
             </div>
         </div>
     );
